0efdf7f62ca9dd0949e4f5abad5396a0bc0458fd
DearJolly <jolly2018@126.com>
2019-06-02 18:50:32 +0800

    Test.java

---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+/**
+ * @author Created by diandian
+ * @date 2019/6/2.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
